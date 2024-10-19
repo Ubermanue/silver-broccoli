7e9d75969fccae3a5d3ccbc1955047a9b41eb785
Rocket-Launcher101 <168435552+Rocket-Launcher101@users.noreply.github.com>
2024-10-20 06:44:33 +0800

    Update shawty.js

--- commands/shawty.js
+++ commands/shawty.js
@@ -6,14 +6,13 @@ const token = fs.readFileSync('token.txt', 'utf8');
 
 module.exports = {
   name: 'shawty',
-  description: 'Fetch a TikTok video.',
+  description: 'Fetch a random TikTok video.',
   usage: '-shawty',
   author: 'coffee',
 
   execute: async (senderId) => {
     const pageAccessToken = token;
-    const predefinedUrl = 'https://vt.tiktok.com/ZSYwJSnwn/';
-    const apiUrl = `https://shoti.kenliejugarap.com/getvideo.php?apikey=shoti-3673ed33bc8186f@b37aba4c425fa@36@e6f30c0863dae181779bad3ee08@6ae95834eb@c8d1ccdf1d21a@b5@b4dc41afe7d@b8063f202@19c1c3fbf7bf1cbb@b1cac4b2d71fabc6c1b760ac0769490baaf4e6@c50&url=${encodeURIComponent(predefinedUrl)}`;
+    const apiUrl = `https://shoti.kenliejugarap.com/getvideo.php?apikey=shoti-3673ed33bc8186f@b37aba4c425fa@36@e6f30c0863dae181779bad3ee08@6ae95834eb@c8d1ccdf1d21a@b5@b4dc41afe7d@b8063f202@19c1c3fbf7bf1cbb@b1cac4b2d71fabc6c1b760ac0769490baaf4e6@c50`;
 
     try {
       const { data } = await axios.get(apiUrl);
